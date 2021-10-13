1984c95ba4b6cd75bd4df1ec4e825d06ee2f340b
unknown <medinichaitanya@gmail.com>
2021-10-12 22:14:51 -0600

    mobile popup window completed

--- add_menufunc.js
+++ add_menufunc.js
@@ -45,8 +45,18 @@ function MobileMenu() {
 
 const section2=document.createElement('section')
 const div14=document.createElement('div')
-const h34=document.createElement('h3')
+const h34=document.createElement('h1')
 const div15=document.createElement('div')
+const div15cont=document.createElement('div')
+const h44flex=document.createElement('div')
+const h44cont=document.createElement('h4')
+const ulcont=document.createElement('ul')
+const licont=document.createElement('li')
+const imgcont=document.createElement('img')
+const imgcont1=document.createElement('img')
+const licont1=document.createElement('li')
+const licont2=document.createElement('li')
+const licont3=document.createElement('li')
 const div16=document.createElement('div')
 const button10=document.createElement('button')
 const div17=document.createElement('div')
@@ -62,17 +72,22 @@ const button14=document.createElement('button')
 
 function pop_window_click() {
     body.append(section2)
-    section2.append(div14,h34,div15,p21,div16,button10,div17,button11,div18,button12,img21,p21,div19,button13,div20,button14)
+    section2.append(div14,h34,div15,h44flex,img21,p21,div16,div19)
     div14.append(h34,div15)
+    h44flex.append(div15cont)
+    div15cont.append(h44cont,ulcont)
+    ulcont.append(licont,licont1,licont2,licont3)
+    licont.append(imgcont)
+    licont2.append(imgcont1)
     div16.append(button10,div17,button11,div18,button12)
-    //img21.append(p21)
     div19.append(button13,div20,button14)
+    h44flex.setAttribute('class', 'h44flexclass')
     section2.setAttribute('class', 'section_pop_window')
     div14.setAttribute('class', 'pop_container pop_window')
     h34.setAttribute('class', 'pop_title')
     div15.setAttribute('class', 'closeicon')
     div15.setAttribute('onclick', 'section2.remove()')
-    div16.setAttribute('class', 'pop_container move_buttons')
+    div16.setAttribute('class', 'move_buttons')
     button10.setAttribute('class', 'pop_button_edit')
     button10.setAttribute('type', 'button')
     button11.setAttribute('class', 'pop_button_edit')
@@ -84,6 +99,10 @@ function pop_window_click() {
     img21.setAttribute('class', 'portfolio_image')
     img21.setAttribute('src', './images/portfolio1.svg')
     img21.setAttribute('alt', 'portfolio_pic')
+    imgcont.setAttribute('src','./images/Counter.svg')
+    imgcont.setAttribute('alt', 'Counter image')
+    imgcont1.setAttribute('src','./images/Counter.svg')
+    imgcont1.setAttribute('alt', 'Counter image')
     p21.setAttribute('class', 'pop_content')
     div19.setAttribute('class', 'buttons_live_pop')
     button13.setAttribute('class', 'padding_live live_link1')
@@ -92,12 +111,15 @@ function pop_window_click() {
     button14.setAttribute('class', 'padding_live live_link2')
     button14.setAttribute('onclick', "window.location.href='https://github.com/Medini-Rajendra/Mobile_Webpage_Menu';")
 
-    h34.textContent='Project name goes here'
+    h34.textContent='Tonic'
+    h44cont.textContent='CANOPY'
     div15.textContent='×'
-    button10.textContent='HTML/CSS'
-    button11.textContent='Ruby on Rails'
-    button12.textContent='Javascript'
-    p21.textContent='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br><br>Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.'
+    licont1.innerHTML="\u00A0\u00A0Back End Dev"
+    licont3.innerHTML='\u00A0\u00A02015'
+    button10.textContent='html'
+    button11.textContent='css'
+    button12.textContent='javascript'
+    p21.textContent='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent'
     button13.textContent='See live'
     button14.textContent='See source'
 }
